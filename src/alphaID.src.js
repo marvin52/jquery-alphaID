affab83f6ea50d21f852ef98dd20ff972e0292ed
Marcelo Boeira <marceloboeira@live.com>
2014-05-28 15:36:16 -0300

    Starting the Plugin Code!
    Signed-off-by: Marcelo Boeira <marceloboeira@live.com>

---
+++ src/alphaID.src.js
@@ -0,0 +1,13 @@
+/*!
+ * jQuery AlphaID  - v0.0.1
+ * ------------------------
+ * Developed by @b03ir4 & @musicompositor
+ * Licensed under MIT (https://github.com/marceloboeira/jquery-alphaID/blob/master/LICENSE)
+ * ---------------------------------------------
+ * Fork us at @github https://github.com/marceloboeira/jquery-alphaID
+ * Found a bug? suggestion? Please open an issue at https://github.com/marceloboeira/jquery-alphaID/issues
+!*/
+
+if (typeof jQuery === 'undefined') { 
+	throw new Error('AlphaID\'s JavaScript requires jQuery');
+}
